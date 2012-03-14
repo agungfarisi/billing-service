e9ddf60c885de0113725775d8a69449466500315
I070405 <I070405@TLVD60200094A.dhcp.tlv.sap.corp>
2012-03-14 09:08:53 +0200

    xxx
    Change-Id: I51054c85582b6afd7b9728367579a749388c3ec5

--- Main/gen/org/billing/service/R.java
+++ Main/gen/org/billing/service/R.java
@@ -11,13 +11,25 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int chair_256=0x7f020000;
+        public static final int chair_48=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int user_48=0x7f020003;
+    }
+    public static final class id {
+        public static final int mainLayout=0x7f050000;
+        public static final int numberOfSeatsLabel=0x7f050002;
+        public static final int numberOfSeatsSetButton=0x7f050004;
+        public static final int numberOfSeatsText=0x7f050003;
+        public static final int toggleNumberOfSeatsButton=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int numberOfSears=0x7f040001;
+        public static final int numberOfSeatsSet=0x7f040002;
+        public static final int numberOfSeatsToggleButtonText=0x7f040003;
     }
 }
